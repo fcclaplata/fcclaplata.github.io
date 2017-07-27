daf3a5eaf946f78968e44c8331dc5bbb3ce70685
Matias Pierobon <matias.pierobonm@gmail.com>
2017-07-27 02:59:30 -0300

    add responsive intent

--- src/components/HomePage/Banner/Banner.jsx
+++ src/components/HomePage/Banner/Banner.jsx
@@ -4,7 +4,7 @@ import './Banner.css'
 export default () => (
     <div className="banner home-banner">
         <div className="banner-text">
-            <h1>FreeCodeCamp</h1>
+            <h1>Free Code Camp</h1>
             <h2>La Plata</h2>
             <p>A Place to Code</p>
         </div>
