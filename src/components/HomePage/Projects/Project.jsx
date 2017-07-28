9528e360de69d05ed817d1f7f0e0905c99c9d107
Matias Pierobon <matias.pierobonm@gmail.com>
2017-07-28 19:34:26 -0300

    add icons of languages to projects

--- src/components/HomePage/Projects/Project.jsx
+++ src/components/HomePage/Projects/Project.jsx
@@ -1,14 +1,40 @@
-import React from 'react'
+import React from "react";
 import ReactMarkdown from "react-markdown";
-import { Button } from 'antd'
+import { Button, Tooltip } from "antd";
 
+const icons = {
+  Python: "devicon-python-plain",
+  HTML: "devicon-html5-plain",
+  CSS: "devicon-css3-plain",
+  Go: "devicon-go-plain",
+  JavaScript: "devicon-javascript-plain"
+};
 
-export default({image, title, repo, body, opened, key }) => (
-    <div className="project">
-      <div className="info">
-        <h1>{title}</h1>
-        <ReactMarkdown className="body" source={body} />
-        <a href={repo} target={`project-${key}`}><Button icon="github" type="primary">Repository</Button></a>
-      </div>
+const getIcon = lang => icons[lang] || null;
+
+export default ({ image, title, repo, body, languages, opened, key }) =>
+  <div className="project">
+    <div className="info">
+      <h1>
+        {title}
+      </h1>
+      <ReactMarkdown className="body" source={body} />
+      <a href={repo} target={`project-${key}`}>
+        <Button icon="github" type="primary">
+          Repository
+        </Button>
+      </a>
+      <ul className="langs">
+        {languages &&
+          languages.map(
+            ({ name, color }, key) =>
+              getIcon(name) &&
+              <Tooltip title={name} key={key}>
+                <li>
+                  <i style={{ color }} className={getIcon(name)}/>
+                </li>
+              </Tooltip>
+          )}
+      </ul>
     </div>
-)
+  </div>;
--- src/components/HomePage/Projects/ProjectList.jsx
+++ src/components/HomePage/Projects/ProjectList.jsx
@@ -1,55 +1,72 @@
-import React, { PureComponent } from "react"
-import { Row, Col } from "antd"
-import { query } from '../../../lib/github'
-import Project from "./Project"
-import projects from "./projects.json"
+import React, { PureComponent } from "react";
+import { Row, Col } from "antd";
+import { query } from "../../../lib/github";
+import Project from "./Project";
+import projects from "./projects.json";
 
-const mapProject = ({ name, url, description }) => ({
-  name, url, description
-})
+const mapProject = ({ name, url, description, languages }) => ({ name, url, description, languages: languages.edges.map( ({ node }) => ({ ...node }) ) });
 
 class ProjectList extends PureComponent {
-
-  constructor(props){
-    super(props)
-    this.state = { projects: null }
+  constructor(props) {
+    super(props);
+    this.state = {
+      projects: null
+    };
   }
 
   componentDidMount() {
-    this.getProjects()
+    this.getProjects();
   }
 
-  getProjects = () => (
+  getProjects = () =>
     query(`query {
        organization(login: "fcclaplata") {
          pinnedRepositories(first:6) { 
            edges { 
             node { 
-               name,
-               url,
-               description
+              name,
+              url,
+              description,
+              languages(first: 5) {
+                edges {
+                  node {
+                    color
+                    name
+                  }
+                }
+              }
             }
           }
         } 
       }
     }`)
-      .then( response => response.data )
-      .then( ({ data }) => data.organization.pinnedRepositories.edges )
-      .then( nodes => this.setState({ projects: nodes.map( ({ node }) => mapProject(node)) }) )
-      .catch(e => console.error(e))
-  )
+      .then(response => response.data)
+      .then(({ data }) => data.organization.pinnedRepositories.edges)
+      .then(nodes =>
+        this.setState({
+          projects: nodes.map(({ node }) => mapProject(node))
+        })
+      )
+      .catch(e => console.error(e));
 
   getChildern = () =>
-    this.state.projects && this.state.projects.map(({ name, description, url }, key) =>
-      <Col xs={24} sm={12} md={8} lg={6} key={key} >
-        <Project title={name} body={description} repo={url} key={key} />
+    this.state.projects &&
+    this.state.projects.map(({ name, description, url, languages }, key) =>
+      <Col xs={24} sm={12} md={8} lg={6} key={key}>
+        <Project title={name} body={description} languages={languages} repo={url} key={key} />
       </Col>
-    )
+    );
 
   render = () =>
-    <Row type="flex" align="middle" justify="space-around" gutter={24} className="project-list">
+    <Row
+      type="flex"
+      align="middle"
+      justify="space-around"
+      gutter={24}
+      className="project-list"
+    >
       {this.getChildern()}
-    </Row>
+    </Row>;
 }
 
 export default ProjectList;
