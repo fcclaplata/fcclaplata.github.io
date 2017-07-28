1d8ea7615f4469cebca3b2536947dfa6ed0ea689
Matias Pierobon <matias.pierobonm@gmail.com>
2017-07-28 19:41:59 -0300

    change timeout to 10s

--- src/lib/github.js
+++ src/lib/github.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const github = axios.create({
   baseURL: 'https://api.github.com/',
-  timeout: 1000,
+  timeout: 10000,
   headers: {
     "Authorization": "bearer 0f615491123211b03e3fe8d16dc986018b41275d"
   }
