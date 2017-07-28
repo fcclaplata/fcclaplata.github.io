2e9da829b7b61f937e8257f64fc372f4f52a893c
Matias Pierobon <matias.pierobonm@gmail.com>
2017-07-28 08:00:49 -0300

    export Project Component

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,5 +1,6 @@
-export {default} from './HomePage'
-export { default as Banner } from './Banner/Banner'
-export { default as Technologies } from './Technologies/Technologies'
-export { default as Architecture } from './Architecture/Architecture'
-export { default as NextMeetUp } from './NextMeetUp/NextMeetUp'
+export { default } from "./HomePage";
+export { default as Banner } from "./Banner/Banner";
+export { default as Technologies } from "./Technologies/Technologies.jsx";
+export { default as Architecture } from "./Architecture/Architecture";
+export { default as NextMeetUp } from "./NextMeetUp/NextMeetUp";
+export { default as Projects } from "./Projects/Projects.jsx";
